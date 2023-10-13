9e191c9b7024e8d701b0a14d08e94fcd2d566b2b
Santiago Valenzuela <sa_valens@hotmail.com>
2023-10-13 02:25:39 -0500

    Finished basic readings for all possible

--- read_altitude/read_altitude.ino
+++ read_altitude/read_altitude.ino
@@ -5,6 +5,7 @@
 */
 
 #include <Wire.h>
+#include <math.h>
 
 
 int32_t t_fine;
@@ -16,7 +17,10 @@ int16_t signed_digP[10]; //dig_P2 to P9; Array[0] and array[1] = 0 for simplicit
 
 #define bmp280add 0x76
 
-int32_t rawTemp;  
+int32_t rawTemp, rawPressure;
+
+float referenceTemperature = 27.1;
+float referencePressure;
 
 void setup() {
   // put your setup code here, to run once:
@@ -31,18 +35,33 @@ void setup() {
   //Initializes configuration of oversamplings and power
   Wire.beginTransmission(bmp280add);
   Wire.write(0xF4);
-  Wire.write(0x53); //0x53 to set x2 Temp-oversampling; x8 pressure-oversampling and normal power-mode 
+  Wire.write(0x57); //0x53 to set x2 Temp-oversampling; x16 pressure-oversampling and normal power-mode 
   Wire.endTransmission();
 
   Wire.beginTransmission(bmp280add);
   Wire.write(0xF5);
-  Wire.write(0x48); //Set standby to 125ms and filter to 8. Finally to 00 bits we ignore cause they are for serial communication
+  Wire.write(0x14); //Set standby to sandard and filter to 16w. Finally to 00 bits we ignore cause they are for serial communication
   Wire.endTransmission();
 
   delay(100);
 
   readCoefficients();
-  readTemperature();
+
+  getReferencePressure();
+}
+
+void getReferencePressure() {
+  float pressures = 0;
+  int counter = 0;
+  for(int i = 0; i < 250; i++) {
+    pressures += readPressure(0);
+    counter++;
+    delay(50);
+  }
+  referencePressure = pressures/counter;
+  Serial.print("Reference pressure: ");
+  Serial.println(referencePressure);
+  delay(10000);
 }
 
 void readCoefficients() {
@@ -63,12 +82,20 @@ void readCoefficients() {
   }
 }
 
-float readTemperature() {
-  Wire.beginTransmission(bmp280add);
-  Wire.write(0xFA);
-  Wire.endTransmission();
-  Wire.requestFrom(bmp280add, 3);
-  if(Wire.available() == 3) {
+float readTemperature(bool for_pressure) {
+  if(for_pressure) {  
+    Wire.beginTransmission(bmp280add);
+    Wire.write(0xF7);
+    Wire.endTransmission();
+    Wire.requestFrom(bmp280add, 6);
+    rawPressure = (uint32_t(Wire.read()) << 16 | uint32_t(Wire.read()) << 8 | uint32_t(Wire.read())) >> 4;
+  } else {
+    Wire.beginTransmission(bmp280add);
+    Wire.write(0xFA);
+    Wire.endTransmission();
+    Wire.requestFrom(bmp280add, 3);
+  }
+  if(Wire.available() >= 3) {
     /*  
         * We call uint32_t to be able to translate 16 bit left-wise on every byte. 
         It has to be unsigned cause that's how the data is given according to the datasheet
@@ -84,13 +111,49 @@ float readTemperature() {
     t_fine  = var1 + var2;
 
     T = (t_fine * 5 + 128) >> 8;
-    return float(T) / 100;
+
+    float referenceTemperature = float(T) / 100;
+    return referenceTemperature;
   }
 }
 
+float readPressure(bool for_altitude) {
+  //If for_altitude = 1 returns altitude in meters
+  //Othewise it returns the pressure in Pa
+  readTemperature(1);
+  int64_t var1, var2, p;
+
+  var1 = ((int64_t)t_fine) - 128000;
+  var2 = var1 * var1 * (int64_t)signed_digP[6];
+  var2 = var2 + ((var1 * (int64_t)signed_digP[5]) << 17);
+  var2 = var2 + (((int64_t)signed_digP[4]) << 35);
+  var1 = ((var1 * var1 * (int64_t)signed_digP[3]) >> 8) +
+         ((var1 * (int64_t)signed_digP[2]) << 12);
+  var1 =
+      (((((int64_t)1) << 47) + var1)) * ((int64_t)dig_P1) >> 33;
+
+  if (var1 == 0) {
+    return 0; // avoid exception caused by division by zero
+  }
+  p = 1048576 - rawPressure;
+  p = (((p << 31) - var2) * 3125) / var1;
+  var1 = (((int64_t)signed_digP[9]) * (p >> 13) * (p >> 13)) >> 25;
+  var2 = (((int64_t)signed_digP[8]) * p) >> 19;
+
+  p = ((p + var1 + var2) >> 8) + (((int64_t)signed_digP[7]) << 4);
+  if(for_altitude) {
+    return readAbsAltitude((float)p / 256);
+  } else {
+    return (float)p / 256; //Returns altitude in pascals
+  }
+}
+
+double readAbsAltitude(double p) {
+  return (log((p/101325.0))/log(2.718281828)) * ((referenceTemperature + 273.15))/(-3.416 / 100);
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(readTemperature());
+  Serial.println(readPressure(1));
   delay(200);
 }
